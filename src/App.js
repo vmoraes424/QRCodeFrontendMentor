7143d51ee24817533182135750c29a881b2af5fc
Vinícius Moraes <49791721+vmoraes424@users.noreply.github.com>
2023-03-31 13:58:32 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="App">
+        <img src="/images/image-qr-code.png" className="qrCode" />
+
+        <h1>Improve you frontend skills by building projects</h1>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </p>
+      </div>
+    </div>
+  );
+}
